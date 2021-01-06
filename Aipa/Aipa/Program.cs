2aa6586eef19ece0e8fbd20179d9c1796cc68661
Dante <Norton.Dante.I@gmail.com>
2021-01-05 20:06:14 -0400

    fdssfd
    dsdsf

---
+++ Aipa/Aipa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aipa.Vista;
+
+namespace Aipa
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+ 
+            Ventana_bienvenida ventana_bienvenida = new Ventana_bienvenida();
+            ventana_bienvenida.ShowDialog();
+            Application.Run(new Ventana_juego());
+        }
+    }
+}
